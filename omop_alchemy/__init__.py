a6530a9b63271532aaff8990986d12d87b6820e6
georgie <georgina.kennedy@unsw.edu.au>
2024-02-19 10:08:02 +1100

    adding handling for environment vars thru dotenv

--- omop_alchemy/__init__.py
+++ omop_alchemy/__init__.py
@@ -6,6 +6,25 @@ from .tables.onco_ext import episode
 from .tables.health_system import Provider
 from .helpers.create_db import create_db 
 
+# grabbing environment variables for path of configuration files - review files .example_dotenv and 
+# oa_system_config_demo.yaml to see what items need to be setup in your local environment
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+message = ""
+try:
+    os.environ['OA_CONFIG']
+except:
+    message = 'Please set OA_CONFIG environment variable to allow loading of db configuration details'
+
+# weird way to do this, but necessary if we are to be able to use jupyter - can't handle exiting from
+# an exception block for some reason...
+if len(message) > 0:
+    raise SystemExit(message)
+
 # this script will create a new sqlite db file with the tables to reflect the specified objects in 
 # clinical and reference directories. needs to be made more friendly for re-create / update etc, but 
 # will do for now :)
