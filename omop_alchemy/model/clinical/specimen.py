1eff75e18084c52ec4269ea14b6557107b584c68
georgie <georgina.kennedy@unsw.edu.au>
2024-02-27 12:37:38 +1100

    correcting FKs in specimen model

--- omop_alchemy/model/clinical/specimen.py
+++ omop_alchemy/model/clinical/specimen.py
@@ -25,10 +25,10 @@ class Specimen(Base):
     person_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('person.person_id', name='s_fk_1'))
     # concept fks
     specimen_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_1'))
-    specimen_type_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_1'))
-    unit_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_1'))
-    anatomic_site_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_1'))
-    disease_status_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_1'))
+    specimen_type_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_2'))
+    unit_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_3'))
+    anatomic_site_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_4'))
+    disease_status_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='s_fk_5'))
     # relationships
     person: so.Mapped[Optional['Person']] = so.relationship(foreign_keys=[person_id])
     # concept_relationships
