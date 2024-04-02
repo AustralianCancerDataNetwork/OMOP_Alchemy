0f7240a9eeffb50e08285442ba2ce50bb762f24f
georgie <georgina.kennedy@unsw.edu.au>
2024-04-03 10:12:13 +1100

    concept_synonym

--- omop_alchemy/model/vocabulary/concept_synonym.py
+++ omop_alchemy/model/vocabulary/concept_synonym.py
@@ -8,5 +8,9 @@ from ...db import Base
 class Concept_Synonym(Base):
     __tablename__ = 'concept_synonym'
     concept_synonym_id: so.Mapped[int] = so.mapped_column(index=True, unique=True, primary_key=True)
+    concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='cs_fk_1'))
     concept_synonym_name: so.Mapped[str] = so.mapped_column(sa.String(1000))
-    language_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='cs_fk_1'), primary_key=True)
+    language_concept_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('concept.concept_id', name='cs_fk_2'))
+
+    synonym_concept: so.Mapped['Concept'] = so.relationship(foreign_keys=[concept_id])
+    language_concept: so.Mapped['Concept'] = so.relationship(foreign_keys=[language_concept_id])
