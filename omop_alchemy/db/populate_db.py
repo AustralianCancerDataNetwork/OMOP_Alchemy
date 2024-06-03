47da21e290e099b482a1b819cfa3c81e36bde61e
georgie <georgina.kennedy@unsw.edu.au>
2024-06-04 09:37:29 +1000

    minor update for data loading spec

--- omop_alchemy/db/populate_db.py
+++ omop_alchemy/db/populate_db.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 import sqlalchemy.sql.sqltypes as sss
+import pandas as pd
 
 from oa_configurator import oa_config, logger
 from ..model.clinical import Person, Condition_Occurrence, Measurement, Observation
@@ -15,38 +16,44 @@ from ..model.vocabulary import Concept, Vocabulary, Concept_Class, Domain, Relat
 # TODO: insert some validation and checks to make sure folk know how and why to do this (and the limits for demo purposes)
 
 to_load_vocabulary = {'folder': 'ohdsi_vocabs',
-                      'VOCABULARY.csv': Vocabulary, 
-                      'CONCEPT.csv': Concept, 
-                      'CONCEPT_CLASS.csv': Concept_Class, 
-                      'DOMAIN.csv': Domain,
-                      'RELATIONSHIP.csv': Relationship,
-                      'CONCEPT_RELATIONSHIP.csv': Concept_Relationship,
-                      'CONCEPT_ANCESTOR.csv': Concept_Ancestor}
+                      'chunkable': False,
+                      'files': {'VOCABULARY.csv': Vocabulary, 
+                                'CONCEPT.csv': Concept, 
+                                'CONCEPT_CLASS.csv': Concept_Class, 
+                                'DOMAIN.csv': Domain,
+                                'RELATIONSHIP.csv': Relationship,
+                                'CONCEPT_RELATIONSHIP.csv': Concept_Relationship,
+                                'CONCEPT_ANCESTOR.csv': Concept_Ancestor}}
 
 to_load_vocab_skip_relationships = {'folder': 'ohdsi_vocabs',
-                                    'VOCABULARY.csv': Vocabulary, 
-                                    'CONCEPT.csv': Concept, 
-                                    'CONCEPT_CLASS.csv': Concept_Class, 
-                                    'DOMAIN.csv': Domain,
-                                    'RELATIONSHIP.csv': Relationship}
+                                    'chunkable': True,
+                                    'files':{'VOCABULARY.csv': Vocabulary, 
+                                             'CONCEPT.csv': Concept, 
+                                             'CONCEPT_CLASS.csv': Concept_Class, 
+                                             'DOMAIN.csv': Domain,
+                                             'RELATIONSHIP.csv': Relationship}}
 
 to_load_just_concept_relationships = {'folder': 'ohdsi_vocabs',
-                                      'CONCEPT_RELATIONSHIP.csv': Concept_Relationship}
+                                     'chunkable': True,
+                                     'files':{'CONCEPT_RELATIONSHIP.csv': Concept_Relationship}}
 
 to_load_just_ancestry = {'folder': 'ohdsi_vocabs',
-                         'CONCEPT_ANCESTOR.csv': Concept_Ancestor}
+                         'chunkable': True,
+                         'files':{'CONCEPT_ANCESTOR.csv': Concept_Ancestor}}
 
 to_load_health_system = {'folder': 'demo_data',
-                         'CARE_SITE.csv': Care_Site,
-                         'LOCATION.csv': Location,
-                         'PROVIDER.csv': Provider}
+                         'chunkable': False,
+                         'files':{'CARE_SITE.csv': Care_Site,
+                                  'LOCATION.csv': Location,
+                                  'PROVIDER.csv': Provider}}
 
 to_load_clinical = {'folder': 'demo_data',
-                    'PERSON.csv': Person,
-                    'CONDITION_OCCURRENCE.csv': Condition_Occurrence,
-                    'MEASUREMENT.csv': Measurement,
-                    'OBSERVATION.csv': Observation,
-                    'CONCEPT.csv': Concept}
+                    'chunkable': False,
+                    'files':{'PERSON.csv': Person,
+                             'CONDITION_OCCURRENCE.csv': Condition_Occurrence,
+                             'MEASUREMENT.csv': Measurement,
+                             'OBSERVATION.csv': Observation,
+                             'CONCEPT.csv': Concept}}
 
 # flexible loading of ohdsi vocab files downloaded to the path /data/ohdsi_vocabs
 
@@ -67,8 +74,11 @@ def convert_datetime_col(dt):
     try:
         return datetime_conversion(dt, '%Y%m%d%H%M%S')
     except:
-        return datetime_conversion(dt, '%Y-%m-%d %H:%M:%S')
-
+        try:
+            return datetime_conversion(dt, '%Y-%m-%d %H:%M:%S')
+        except: # workaround for excel annoying behaviour
+            return datetime_conversion(dt, '%Y-%m-%d %H:%M:%S.%f')
+        
 def callable_pass(s):
     return s
 
@@ -112,6 +122,30 @@ def data_load_prep():
 def after_data_load():
     # turn back on foreign keys if they've been turned off
     ...
+
+
+def populate_chunks_from_file(filepath, interface, sess):
+    logger.debug(filepath)
+    try: 
+        field_map = get_type_lookup(interface)
+        base_tables = [b for b in interface.__bases__ if hasattr(b, '__tablename__')]
+        if len(base_tables) > 0:
+            for b in base_tables:
+                field_map.update(get_type_lookup(b))
+
+        for chunk in pd.read_csv(filepath, chunksize=10000, delimiter='\t', low_memory=False):
+            rows = chunk.to_dict(orient='records')
+            records = map(lambda x: {field:field_map[field](data) for field, data in x.items() if field in field_map}, rows)
+            objects = map(lambda x: interface(**x), records)
+            sess.add_all(objects)
+            sess.commit()
+            logger.debug(f'chunk loaded for: {filepath}')
+        
+        logger.debug(f'complete load for: {filepath}')
+    except Exception as e:
+        logger.error(e)
+        logger.debug(f'Error loading data file {filepath}. Have you unzipped it in the correct location ({oa_config.data_path})?')
+
 
 def populate_db_from_file(filepath, interface, session):
     logger.debug(filepath)
@@ -159,7 +193,10 @@ def populate_db_from_dict(to_load):
     with so.Session(oa_config.engine) as sess:
         folder = Path(oa_config.data_path) / to_load['folder']
         logger.debug(folder)
-        for ohdsi_file, interface in to_load.items():
+        for ohdsi_file, interface in to_load['files'].items():
             if interface != folder.name and (folder / ohdsi_file).exists():
-                populate_db_from_file(folder / ohdsi_file, interface, sess)
+                if to_load['chunkable']:
+                    populate_chunks_from_file(folder / ohdsi_file, interface, sess)
+                else:
+                    populate_db_from_file(folder / ohdsi_file, interface, sess)
         sess.commit()
